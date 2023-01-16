589b31bf2c4a4ba0d3f7cd1d628875b6daffb07f
bdke <91001643+bdke@users.noreply.github.com>
2023-01-16 13:11:34 +0800

    Added support for built-in functions
    added client and server (from vscode documentation :+1: )
    added support for current built-in functions

---
+++ src/ts/server.ts
@@ -0,0 +1,156 @@
+import {
+    createConnection,
+    TextDocuments,
+    Diagnostic,
+    DiagnosticSeverity,
+    ProposedFeatures,
+    InitializeParams,
+    DidChangeConfigurationNotification,
+    CompletionItem,
+    CompletionItemKind,
+    TextDocumentPositionParams,
+    TextDocumentSyncKind,
+    InitializeResult,
+} from "vscode-languageserver/node";
+import { TextDocument } from "vscode-languageserver-textdocument";
+import { BuiltInFunctions } from "./data/builtinFunctions";
+
+let connection = createConnection(ProposedFeatures.all);
+let text: string;
+
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability: boolean = false;
+let hasWorkspaceFolderCapability: boolean = false;
+let hasDiagnosticRelatedInformationCapability: boolean = false;
+
+connection.onInitialize((params: InitializeParams) => {
+    let capabilities = params.capabilities;
+
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.configuration
+    );
+    hasWorkspaceFolderCapability = !!(
+        capabilities.workspace && !!capabilities.workspace.workspaceFolders
+    );
+    hasDiagnosticRelatedInformationCapability = !!(
+        capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation
+    );
+
+    const result: InitializeResult = {
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true,
+            },
+        },
+    };
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true,
+            },
+        };
+    }
+    return result;
+});
+
+connection.onInitialized(() => {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(
+            DidChangeConfigurationNotification.type,
+            undefined
+        );
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+            connection.console.log("Workspace folder change event received.");
+        });
+    }
+});
+
+interface ServerSettings {
+    maxNumberOfProblems: number;
+}
+
+const defaultSettings: ServerSettings = { maxNumberOfProblems: 1000 };
+let globalSettings: ServerSettings = defaultSettings;
+
+let documentSettings: Map<string, Thenable<ServerSettings>> = new Map();
+
+connection.onDidChangeConfiguration((change) => {
+    if (hasConfigurationCapability) {
+        documentSettings.clear();
+    } else {
+        globalSettings = <ServerSettings>(
+            (change.settings.languageServerExample || defaultSettings)
+        );
+    }
+
+    documents.all().forEach(validateTextDocument);
+});
+
+function getDocumentSettings(resource: string): Thenable<ServerSettings> {
+    if (!hasConfigurationCapability) {
+        return Promise.resolve(globalSettings);
+    }
+    let result = documentSettings.get(resource);
+    if (!result) {
+        result = connection.workspace.getConfiguration({
+            scopeUri: resource,
+            section: "jmc",
+        });
+        documentSettings.set(resource, result);
+    }
+    return result;
+}
+
+documents.onDidClose((e) => {
+    documentSettings.delete(e.document.uri);
+});
+
+documents.onDidChangeContent((change) => {
+    validateTextDocument(change.document);
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+    let settings = await getDocumentSettings(textDocument.uri);
+
+    text = textDocument.getText();
+    let pattern = /\b[A-Z]{2,}\b/g;
+    let m: RegExpExecArray | null;
+
+    let problems = 0;
+    let diagnostics: Diagnostic[] = [];
+    while ((m = pattern.exec(text)) && problems < 100) {
+        problems++;
+    }
+
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+connection.onDidChangeWatchedFiles((_change) => {
+    connection.console.log("We received a file change event");
+});
+
+connection.onCompletion(
+    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+        var builtinFunctionsName = BuiltInFunctions.map((v) => ({name: v.class}));
+        var items: CompletionItem[] = [];
+        var num = 0;
+        for (let i of builtinFunctionsName) {
+            items.push({label: i.name, kind: CompletionItemKind.Class, data: num});
+            num++;
+        }
+        return items;
+    }
+);
+
+documents.listen(connection);
+connection.listen();
---
+++ src/ts/source.ts
@@ -0,0 +1,78 @@
+import { workspace, ExtensionContext, languages, DocumentSelector, LanguageConfiguration } from "vscode";
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind,
+} from "vscode-languageclient/node";
+import * as path from "path";
+import { BuiltInFunctions } from "./data/builtinFunctions";
+import * as vscode from "vscode";
+
+
+const selector: DocumentSelector = {
+    language: 'jmc',
+    scheme: 'file'
+}
+let client: LanguageClient;
+
+export async function activate(context: ExtensionContext) {
+    //setup client
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: [{scheme: "file", language: "jmc"}],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher("**/.clientsrc")
+        }
+    };
+
+    //setup server
+    let serverModule = context.asAbsolutePath(
+        path.join("src", "js", "server.js")
+    );
+    let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+
+    let serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions,
+        },
+    };
+
+    //define client
+    client = new LanguageClient(
+        "jmc",
+        "Testing",
+        serverOptions,
+        clientOptions
+    )
+
+    const builtinFunctionsCompletion = languages.registerCompletionItemProvider(
+        selector,
+        {
+            provideCompletionItems(document, position, token, context) {
+                const linePrefix = document.lineAt(position).text.substring(0, position.character);
+                for (let i of BuiltInFunctions) {
+                    if (linePrefix.endsWith(`${i.class}.`)) {
+                        var methods: vscode.CompletionItem[] = [];
+                        for (let method of i.methods) {
+                            methods.push(new vscode.CompletionItem(method, vscode.CompletionItemKind.Method));
+                        }
+                        return methods;
+                    }
+                }
+                return undefined;
+            }
+        },
+        "."
+    )
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
