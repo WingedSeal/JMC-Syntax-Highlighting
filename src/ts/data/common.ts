68bf504e10ed0485fee0190a762c3c90310a0604
bdke <91001643+bdke@users.noreply.github.com>
2023-01-18 15:45:50 +0800

    Added Snippets, auto completion for user defined variables and functions

---
+++ src/ts/data/common.ts
@@ -0,0 +1,8 @@
+interface KeywordsSnippets {
+    keyword: string,
+    snippet: string
+}
+
+export const keywords: Array<string> = [
+    "function","new","if","else","while","for","do","switch","case"
+]
