d2f752a5799ad78a18d5ea65f320558169d55b91
bdke <91001643+bdke@users.noreply.github.com>
2023-09-11 20:59:47 +0800

    Added unittesting

---
+++ JMC.Extension.Server.Tests/Lexer/JMCLexerTest.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using JMC.Extension.Server.Lexer.JMC;
+using JMC.Extension.Server.Lexer.JMC.Types;
+using Xunit;
+
+namespace JMC.Extension.Server.Tests.Lexer
+{
+    //TODO: test case for commands
+    public class JMCLexerTest
+    {
+        [Theory]
+        [InlineData("function", JMCTokenType.FUNCTION)]
+        [InlineData("switch", JMCTokenType.SWITCH)]
+        [InlineData("case", JMCTokenType.CASE)]
+        [InlineData("if", JMCTokenType.IF)]
+        [InlineData("else", JMCTokenType.ELSE)]
+        [InlineData("do", JMCTokenType.DO)]
+        [InlineData("while", JMCTokenType.WHILE)]
+        [InlineData("new", JMCTokenType.NEW)]
+        [InlineData("class", JMCTokenType.CLASS)]
+        public void Keyword_Tests(string text, JMCTokenType expected)
+        {
+            var type = JMCLexer.GetTokenType(text);
+            Assert.Equal(expected, type);
+        }
+
+        [Theory]
+        [MemberData(nameof(JMCLexerTestCase.StatementTestData), MemberType = typeof(JMCLexerTestCase))]
+        public void Statement_Tests(string text, IEnumerable<JMCTokenType> expected)
+        {
+            var lexer = new JMCLexer(text);
+            var tokenTypes = lexer.Tokens.Select(v => v.TokenType).ToArray();
+            expected.Should().Equal(tokenTypes);
+        }
+
+        [Theory]
+        [InlineData("123", JMCTokenType.NUMBER)]
+        [InlineData("1.23", JMCTokenType.NUMBER)]
+        [InlineData("//test comment", JMCTokenType.COMMENT)]
+        [InlineData("$test", JMCTokenType.VARIABLE)]
+        [InlineData("$test.get()", JMCTokenType.VARIABLE_CALL)]
+        public void SpecialCases_Tests(string text, JMCTokenType expected)
+        {
+            var type = JMCLexer.GetTokenType(text);
+            Assert.Equal(expected, type);
+        }
+
+        [Theory]
+        [InlineData(@"class test{function test(){if (tag) {}}function test2(){test.test2();}}", "test2 test.test2")]
+        public void FormatFunctions_Tests(string text, string expected)
+        {
+            var lexer = new JMCLexer(text);
+            var functionDefine = lexer.FunctionDefines.ElementAt(1);
+            functionDefine.Value.Should().Be(expected);
+        }
+    }
+}
---
+++ JMC.Extension.Server.Tests/Lexer/JMCLexerTestCase.cs
@@ -0,0 +1,67 @@
+using JMC.Extension.Server.Lexer.JMC.Types;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMC.Extension.Server.Tests.Lexer
+{
+    public class JMCLexerTestCase
+    {
+        public static IEnumerable<object[]> StatementTestData => new List<object[]>
+        {
+            //function test
+            new object[]
+            {
+                "function test() {}", new JMCTokenType[]
+                {
+                    JMCTokenType.FUNCTION, JMCTokenType.LITERAL,
+                    JMCTokenType.LPAREN, JMCTokenType.RPAREN,
+                    JMCTokenType.LCP, JMCTokenType.RCP,
+                }
+            },
+            //class test
+            new object[]
+            {
+                "class name.name {}", new JMCTokenType[]
+                {
+                    JMCTokenType.CLASS, JMCTokenType.LITERAL, JMCTokenType.LCP, JMCTokenType.RCP
+                }
+            },
+            //switch test
+            new object[]
+            {
+                "switch ($test) {case 1: test();}", new JMCTokenType[]
+                {
+                    JMCTokenType.SWITCH, JMCTokenType.LPAREN, JMCTokenType.VARIABLE, JMCTokenType.RPAREN,
+                    JMCTokenType.LCP,
+                    JMCTokenType.CASE, JMCTokenType.NUMBER, JMCTokenType.COLON,
+                    JMCTokenType.LITERAL, JMCTokenType.LPAREN, JMCTokenType.RPAREN, JMCTokenType.SEMI,
+                    JMCTokenType.RCP,
+                }
+            },
+            //comment test
+            new object[]
+            {
+                "//test comment\r\ntest();", new JMCTokenType[]
+                {
+                    JMCTokenType.COMMENT, JMCTokenType.LITERAL, JMCTokenType.LPAREN, JMCTokenType.RPAREN, JMCTokenType.SEMI
+                }
+            }
+            //new object[]
+            //{
+            //    "if ($test == 3) {} else {}", new JMCTokenType[]
+            //    {
+            //        JMCTokenType.IF,
+            //        JMCTokenType.LPAREN, JMCTokenType.VARIABLE, JMCTokenType.OP_EQUAL, JMCTokenType.NUMBER ,JMCTokenType.RPAREN,
+            //        JMCTokenType.LCP, JMCTokenType.RCP,
+            //        JMCTokenType.ELSE,
+            //        JMCTokenType.LCP, JMCTokenType.RCP
+            //    }
+            //}
+        };
+
+    }
+}
