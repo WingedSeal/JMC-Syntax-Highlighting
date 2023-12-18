461a53be000e1526161d6064ac6026cbc8a6e953
bdke <91001643+bdke@users.noreply.github.com>
2023-12-18 16:19:15 +0800

    Update publish.bat

--- publish.bat
+++ publish.bat
@@ -1,9 +1,11 @@
+@echo off
 cd .\vscode-ext
-npm install
-tsc --build tsconfig.json
+call npm install
+call tsc --build tsconfig.json
 cd ..\
-dotnet publish JMC.Extension.Server -p:PublishProfile=Windows -o .\vscode-ext\out\src\server -v q
-dotnet publish JMC.Extension.Server -p:PublishProfile=macOS -o .\vscode-ext\out\src\server -v q
+call dotnet publish JMC.Extension.Server -p:PublishProfile=Windows -o .\vscode-ext\out\src\server -v q
+call dotnet publish JMC.Extension.Server -p:PublishProfile=macOS -o .\vscode-ext\out\src\server -v q
 cd .\vscode-ext
-vsce publish
+call vsce publish
 cd ..\
+pause
